52c203eebbb81016097728cd8cdf8876c8f5b8c4
Kai Zhu <Kevin.zhu.cn@outlook.com>
2017-03-15 02:31:54 -0500

    3.15.1

---
+++ src/app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Kaiz on 3/15/17.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Kaiz on 3/15/17.
+ */
+
+
+export class Hero{
+  id: number;
+  name: string;
+}
